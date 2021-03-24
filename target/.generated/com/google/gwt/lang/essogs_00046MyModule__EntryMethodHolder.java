4d775293b3e93897759b0ac07bb1b25422680a0b
kash2093 <kashdawar0081@gmail.com>
2021-03-24 16:35:18 -0700

    this is for my new war file essonew

---
+++ target/.generated/com/google/gwt/lang/essogs_00046MyModule__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class essogs_00046MyModule__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
---
+++ target/.generated/org/cruxframework/crux/core/client/ioc/IocContainer_main_largeDisplayTouch.java
@@ -0,0 +1,26 @@
+package org.cruxframework.crux.core.client.ioc;
+
+import org.cruxframework.crux.core.client.screen.views.View;
+import com.google.gwt.core.client.GWT;
+
+public class IocContainer_main_largeDisplayTouch extends org.cruxframework.crux.core.client.ioc.IocContainer {
+  public IocContainer_main_largeDisplayTouch(View view){
+    super(view);
+  }
+  public  essogs.client.MainController.MainView getessogs_client_MainController_MainView(org.cruxframework.crux.core.client.ioc.IoCResource.Scope scope, String subscope){
+    essogs.client.MainController.MainView result = _getScope(scope).getValue(new org.cruxframework.crux.core.client.ioc.IocProvider<essogs.client.MainController.MainView>(){
+      public essogs.client.MainController.MainView get(){
+        return GWT.create(essogs.client.MainController.MainView.class);
+      }
+    }, "essogs.client.MainController.MainView", subscope, 
+    new IocScope.CreateCallback<essogs.client.MainController.MainView>(){
+      public void onCreate(essogs.client.MainController.MainView newObject){
+      }
+    }
+    );
+    if (scope != org.cruxframework.crux.core.client.ioc.IoCResource.Scope.SINGLETON && result.getBoundCruxViewId() == null){
+      result.bindCruxView(this.getBoundCruxViewId());
+    }
+    return result;
+  }
+}
